136f2df2d340f7ce848ef27e6923b582c5c7955f
Gautam Kumar <gauta@amazon.com>
2024-07-16 01:26:55 -0700

    Adding complete pre-training

--- gpt2.py
+++ gpt2.py
@@ -212,25 +212,41 @@ class GPT(nn.Module):
         
 # -----------------------------------------------------------------------------
 import tiktoken
+import numpy as np
 
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
 
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_processes):
+    def __init__(self, B, T, process_rank, num_processes, split):
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.num_processes = num_processes
-        with open('input.txt', 'r') as f:
-            text = f.read()
+        assert split in {'train', 'val'}
+
         
-        enc = tiktoken.get_encoding('gpt2')
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f"loaded {len(self.tokens)} tokens")
-        print(f"1 epoch = {len(self.tokens) // (B * T)} batches") 
-        
-        # state 
-        self.current_position = self.B * self.T * self.process_rank
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+   
+         self.reset()
+
+    def reset(self):
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank     
+
         
     def next_batch(self):
         B, T = self.B, self.T
@@ -243,6 +259,8 @@ class DataLoaderLite:
         
         # if loading the next batch out of bound, reset 
         if self.current_position + (B * T *self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
             self.current_position = self.B * self.T * self.process_rank
             
         return x , y
@@ -304,7 +322,8 @@ if master_process:
 
 
 
-train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size)
+train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="train")
+val_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="val")
 
 # TF32
 torch.set_float32_matmul_precision('high')
@@ -318,10 +337,10 @@ if ddp:
 
 raw_model = model.module if ddp else model #always contains the raw unwrapped model 
     
-max_lr = 3e-4 
+max_lr = 6e-4 
 min_lr = max_lr * 0.1
-warmup_steps = 10 
-max_steps = 50
+warmup_steps = 715 # refer GPT paper 375Milliontoken/total_batch_size
+max_steps = 19073 # 10B / total_batch_size
 
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
